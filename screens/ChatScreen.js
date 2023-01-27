be64d8120b115e77b50091076cd382bb06d9fcfe
mrmallam <96600566+mrmallam@users.noreply.github.com>
2023-01-27 15:23:16 -0700

    Chat functionality update. (still not complete)

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import { Keyboard, KeyboardAvoidingView, Platform, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
 import { Avatar } from 'react-native-elements';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
+import { db, auth } from '../firebase';
+import firebase from 'firebase/compat/app';
 
 const ChatScreen = ({ navigation, route }) => {
+
+    const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -19,22 +26,198 @@ const ChatScreen = ({ navigation, route }) => {
                 </View>
             ),
             headerLeft: () => (
-                <TouchableOpacity>
+                <TouchableOpacity
+                    style={{ marginLeft: 10 }}
+                    onPress={navigation.goBack}
+                >
                     <AntDesign name="arrowleft" size={24} color="white" />
                 </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width: 70,
+                    }}
+                >
+                    <TouchableOpacity>
+                        <FontAwesome5 name="video" size={24} color="white" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="md-call" size={24} color="white" />
+                    </TouchableOpacity>
+                </View>
             )
         });
     }, [navigation]);
 
+    const sendMessage = () => {
+        Keyboard.dismiss();
+        db.collection("chats").doc(route.params.id).collection("messages").add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            displayName: auth.currentUser.displayName,
+            email: auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL
+        });
+        setInput('');
+    };
+
+    useLayoutEffect(() => {
+        const unsubscribe = db
+            .collection("chats")
+            .doc(route.params.id)
+            .collection("messages")
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) => setMessages(
+                snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                }))
+            ));
+        return unsubscribe;
+    }, [route]);
+
+
     return (
-        <View>
-            <Text>{route.params.chatName}</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+            <StatusBar style="light" />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? 'padding' : 'height'}
+                style={styles.container}
+                keyboardVerticalOffset={90}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <>
+                        <ScrollView>
+                            {messages.map(({ id, data }) => (
+                                data.email = auth.currentUser.email ? (
+                                    <View key={id} style={styles.reciever}>
+                                        <Avatar
+                                            position="absolute"
+                                            rounded
+                                            // For Web
+                                            containerStyle={{
+                                                position: 'absolute',
+                                                bottom: -15,
+                                                right: -5,
+                                            }}
+                                            bottom={-15}
+                                            right={-15}
+                                            size={30}
+                                            source={{
+                                                uri: data.photoURL,
+                                            }}
+                                        />
+                                        <Text style={styles.recieverText}> {data.message} </Text>
+                                    </View>
+
+                                ) : (
+                                    <View style={styles.sender}>
+                                        <Avatar
+                                            position="absolute"
+                                            rounded
+                                            // For Web
+                                            containerStyle={{
+                                                position: 'absolute',
+                                                bottom: -15,
+                                                right: -5,
+                                            }}
+                                            bottom={-15}
+                                            right={-15}
+                                            size={30}
+                                            source={{
+                                                uri: data.photoURL,
+                                            }}
+
+                                        />
+                                        <Text style={styles.senderText}> {data.message} </Text>
+                                        <Text style={styles.senderName}> {data.displayName} </Text>
+                                    </View>
+
+                                )
+                            ))}
+                        </ScrollView>
+                        <View style={styles.footer}>
+                            <TextInput
+                                value={input}
+                                onChangeText={(text) => setInput(text)}
+                                onSubmitEditing={sendMessage}
+                                placeholder='Signal Message'
+                                style={styles.textInput}
+                            />
+                            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5} style={{ marginBottom: 10 }}>
+                                <Ionicons name='send' size={24} color='#2B68E6' />
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    footer: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: "100%",
+        padding: 15,
+    },
+    textInput: {
+        bottom: 0,
+        height: 40,
+        flex: 1,
+        marginRight: 15,
+        backgroundColor: "#ECECEC",
+        padding: 10,
+        color: "grey",
+        borderRadius: 30,
+        marginBottom: 10,
+    },
+    recieverText: {
+        color: "black",
+        fontWeight: "500",
+        marginLeft: 10,
+    },
+    senderText: {
+        color: "white",
+        fontWeight: "500",
+        marginLeft: 10,
+        marginBottom: 15,
+    },
+    reciever: {
+        padding: 15,
+        backgroundColor: "#ECECEC",
+        alignSelf: "flex-end",
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 15,
+        maxWidth: "80%",
+        position: "relative",
+
+    },
+    sender: {
+        padding: 15,
+        backgroundColor: "#2B68E6",
+        alignSelf: "flex-start",
+        borderRadius: 20,
+        margin: 15,
+        maxWidth: "80%",
+        position: "relative",
+    },
+    senderName: {
+        left: 10,
+        paddingRight: 10,
+        fontSize: 10,
+        color: "white",
+    },
+})
 
 
